5ebbf77c69dc58bd72fc0a3783b67b1f63ca9365
Letícia Gonçalves <leticiaespregagon@gmail.com>
2023-06-29 23:39:20 -0300

    fix: adjust cors

--- src/main/java/com/example/pawsAndCare/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/pawsAndCare/security/SpringSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.pawsAndCare.security;
 
 import com.example.pawsAndCare.domain.model.security.FilterToken;
+import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,13 +28,35 @@ public class SpringSecurityConfiguration {
     private FilterToken filter;
 
     @Bean
+    public Filter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // Configurar as permissões do CORS conforme necessário
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration corsConfig = new CorsConfiguration();
+                    corsConfig.addAllowedOrigin("*");
+                    corsConfig.addAllowedMethod("*");
+                    corsConfig.addAllowedHeader("*");
+                    return corsConfig;
+                }))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/v1/signin").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/admins").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
@@ -42,7 +68,7 @@ public class SpringSecurityConfiguration {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
